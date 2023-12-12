691f9fa4fb95ddbe78515f4dee6ed2c916d7e186
Colin McNaughton <cloin@users.noreply.github.com>
2023-12-12 16:55:09 +0000

    workflow updates

---
+++ .github/workflows/process_matrix.py
@@ -0,0 +1,50 @@
+import json
+import os
+import sys
+import logging
+import yaml
+
+def setup_logger():
+    logger = logging.getLogger('process_matrix')
+    logger.setLevel(logging.INFO)
+    handler = logging.StreamHandler(sys.stdout)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    return logger
+
+def process_matrix(input_path):
+    logger = setup_logger()
+    logger.info(f"Generating EE build matrix for: {input_path}")
+
+    matrix_file = os.path.join(input_path, 'matrix.yml')
+    matrix = {'include': []}
+
+    if os.path.exists(matrix_file):
+        logger.info(f"Found matrix.yml at: {matrix_file}")
+        with open(matrix_file, 'r') as file:
+            versions = yaml.safe_load(file).get('versions', [])
+            for version in versions:
+                matrix['include'].append({
+                    'ee': os.path.basename(input_path),
+                    'version': version.get('name', 'default'),
+                    'base_image': version.get('base_image', '')
+                })
+    else:
+        logger.info("No matrix.yml found. Using default build configuration.")
+        matrix['include'].append({'ee': os.path.basename(input_path), 'version': 'default', 'base_image': ''})
+
+    logger.info(f"Generated matrix: {json.dumps(matrix, indent=4)}")
+    return matrix
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: process_matrix.py <input-path>")
+        sys.exit(1)
+
+    input_path = sys.argv[1]
+    matrix = process_matrix(input_path)
+    print(json.dumps(matrix))
+
+if __name__ == "__main__":
+    main()
